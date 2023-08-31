3cc63b6546de9ebae34337d4bf7e8623ba9193bf
Joana <108015125+JMOgawa@users.noreply.github.com>
2023-08-31 18:45:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,48 @@
 //OBS: código incompleto
 function btnConvert(){
-  var value = document.getElementById("valor-entrada").value;
+  var value = parseFloat(document.getElementById("valor-entrada").value);
   //API: AWESOMEAPI - https://docs.awesomeapi.com.br/api-de-moedas
   //fetch para acessar a API
-  fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL')
+  fetch('https://economia.awesomeapi.com.br/last/USD-BRL,USD-EUR,EUR-BRL')
     .then(response => response.json())
     .then(data => {
-        //console.log(data)
-        var euro = data.EURBRL.high;
-        var dolar = data.USDBRL.high;
-        console.log("Dola: ",dolar);
-        console.log("última Atualização: ", data.USDBRL.create_date);
-        console.log("Euro: ", euro);
-        console.log("última Atualização: ", data.EURBRL.create_date);
+        var euro_reais = data.EURBRL.high;
+        var dolar_reais = data.USDBRL.high;
+        var dolar_euro = data.USDEUR.high;
+        console.log("USDBRL: ",dolar_reais, " - ", data.USDBRL.create_date);
+        console.log("EURBRL: ", euro_reais, " - ", data.EURBRL.create_date);
+        console.log("USDEUR: ", euro_reais, " - ", data.USDEUR.create_date);
         const moeda_entrada = document.getElementById("entrada1").value
         const moeda_saida = document.getElementById("entrada2").value;
-        //alert(moeda)
         var result;
         switch(moeda_entrada) {
           case '1':
             if(moeda_saida=='11')
                 result=value;
             else if(moeda_saida=='22')
-              result=value/dolar;
+              result=value/dolar_reais;
             else if(moeda_saida=='33')
-              result=value/euro;
+              result=value/euro_reais;
             break;
           case '2':
             if(moeda_saida=='11')
-                result=value/dolar;
+                result=value*dolar_reais;
             else if(moeda_saida=='22')
               result=value;
             else if(moeda_saida=='33')
-              result=value/euro;
+              result=value*dolar_euro;
             break;
           case '3':      
             if(moeda_saida=='11')
-                result=value/euro;
+                result=value*euro_reais;
             else if(moeda_saida=='22')
-              result=value/dolar;
+              result=value/dolar_euro;
             else if(moeda_saida=='33')
               result=value;
             break;
           default:
             result = 0.00;
         }
-        document.getElementById("valor-saida").innerHTML = result;
+        document.getElementById("valor-saida").innerHTML = result.toFixed(2);
     }).catch(error => console.log(error));  
 }
